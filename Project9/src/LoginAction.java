bdee77eb4347a99110ae5953521d800e75cff5e8
刘杰 <1684468347@qq.com>
2022-05-14 11:43:47 +0800

    增加数据库语句，需要者可自行修改，默认不使用数据库。

--- Project9/src/LoginAction.java
+++ Project9/src/LoginAction.java
@@ -1,27 +1,36 @@
-
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import dao.Dao;
+import model.User;
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-public class LoginAction extends Action {
-	public LoginAction() {
-		System.out.println("LoginAction构造函数");
-	}
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		LoginForm loginForm = (LoginForm) form;
-		String account = loginForm.getAccount();
-		String password = loginForm.getPassword();
-		String url = null;
-		if (account.equals(password)) {
-			return mapping.findForward("SUCCESS");
-		}
-		return mapping.findForward("FAIL");
-	}
+
+public class LoginAction extends Action {
+    public LoginAction() {
+        System.out.println("LoginAction构造函数");
+    }
+
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+                                 HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        LoginForm loginForm = (LoginForm) form;
+        String account = loginForm.getAccount();
+        String password = loginForm.getPassword();
+        String url = null;
+
+//		User user  = Dao.queryUserByAccount(account);
+//		if ((user != null) && password.equals(user.getPassword())) {
+//			return mapping.findForward("SUCCESS");
+//		}
+
+        if (account.equals(password)) {
+            return mapping.findForward("SUCCESS");
+        }
+
+        return mapping.findForward("FAIL");
+    }
 }
